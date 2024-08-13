6c1b804c476952bae30b17200e5f7e69c586327e
saurabh-rana17 <saurabhrana200317@gmail.com>
2024-08-13 18:59:21 +0530

    routing test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
 import React from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Page1 from "./pages/page1";
+import Layout from "./pages/Layout";
+import Page2 from "./pages/Page2";
+import Page3 from "./pages/Page3";
+import Page4 from "./pages/Page4";
 
 export default function App() {
-  return <div>App</div>;
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/page1" element={<Page1 />} />
+          <Route path="/page2" element={<Page2 />} />
+          <Route path="/page3" element={<Page3 />} />
+          <Route path="/page4" element={<Page4 />} />
+        </Route>
+      </>
+    )
+  );
+
+  return <RouterProvider router={router}></RouterProvider>;
 }
