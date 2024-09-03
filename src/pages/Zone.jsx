2bd8822ddf21659dfb92173e335b6b400fccee18
saurabh-rana17 <saurabhrana200317@gmail.com>
2024-09-03 08:30:50 +0530

    gauge added

--- src/pages/Zone.jsx
+++ src/pages/Zone.jsx
@@ -9,9 +9,10 @@ import {
 } from "@mui/material";
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import React from "react";
-
 import Grid from "@mui/material/Unstable_Grid2"; // Import Grid from MUI
 import { green } from "@mui/material/colors";
+import GaugeComponent from "react-gauge-component";
+
 function BasicCard() {
   return (
     <Grid xs={12} md={4} sm={6} lg={3}>
@@ -35,9 +36,49 @@ function BasicCard() {
         </Box>
         <Divider />
         <CardContent sx={{ padding: "1.5rem", textAlign: "center" }}>
-          <Typography variant="body1" mb={1}>
-            <strong>Level:</strong> 80%
-          </Typography>
+          <div>
+            <GaugeComponent
+              value={80}
+              type="semicircle"
+              labels={{
+                valueLabel: {
+                  matchColorWithArc: true,
+                  style: {
+                    fontSize: "35px",
+                    fill: "#fff",
+                    textShadow:
+                      "none 0px 0px 0px, black 0px 0px 0em, black 0px 0px 0em",
+                    border: "0px",
+                  },
+                },
+                tickLabels: {
+                  type: "outer",
+                  ticks: [
+                    { value: 25 },
+                    { value: 50 },
+                    { value: 75 },
+                    { value: 100 },
+                  ],
+                },
+              }}
+              arc={{
+                colorArray: ["#5BE12C", "#EA4228"],
+                subArcs: [
+                  { limit: 25 },
+                  { limit: 50 },
+                  { limit: 75 },
+                  { limit: 100 },
+                ],
+                padding: 0.02,
+                width: 0.3,
+              }}
+              pointer={{
+                type: "arrow",
+                elastic: true,
+                animationDelay: 0,
+              }}
+            />
+          </div>
           <Typography variant="body2" color="textSecondary" mb={1}>
             <strong>Coordinates:</strong> lat 67.243, long 32.434
           </Typography>
@@ -86,9 +127,49 @@ export default function Zone() {
             </Box>
             <Divider />
             <CardContent sx={{ padding: "1.5rem", textAlign: "center" }}>
-              <Typography variant="body1" mb={1}>
-                <strong>Level:</strong> 30%
-              </Typography>
+              <div>
+                <GaugeComponent
+                  value={25}
+                  type="semicircle"
+                  labels={{
+                    valueLabel: {
+                      matchColorWithArc: true,
+                      style: {
+                        fontSize: "35px",
+                        fill: "#fff",
+                        textShadow:
+                          "none 0px 0px 0px, black 0px 0px 0em, black 0px 0px 0em",
+                        border: "0px",
+                      },
+                    },
+                    tickLabels: {
+                      type: "outer",
+                      ticks: [
+                        { value: 25 },
+                        { value: 50 },
+                        { value: 75 },
+                        { value: 100 },
+                      ],
+                    },
+                  }}
+                  arc={{
+                    colorArray: ["#5BE12C", "#EA4228"],
+                    subArcs: [
+                      { limit: 25 },
+                      { limit: 50 },
+                      { limit: 75 },
+                      { limit: 100 },
+                    ],
+                    padding: 0.02,
+                    width: 0.3,
+                  }}
+                  pointer={{
+                    type: "arrow",
+                    elastic: true,
+                    animationDelay: 0,
+                  }}
+                />
+              </div>
               <Typography variant="body2" color="textSecondary" mb={1}>
                 <strong>Coordinates:</strong> lat 23.243, long 26.434
               </Typography>
@@ -129,9 +210,49 @@ export default function Zone() {
             </Box>
             <Divider />
             <CardContent sx={{ padding: "1.5rem", textAlign: "center" }}>
-              <Typography variant="body1" mb={1}>
-                <strong>Level:</strong> 60%
-              </Typography>
+              <div>
+                <GaugeComponent
+                  value={60}
+                  type="semicircle"
+                  labels={{
+                    valueLabel: {
+                      matchColorWithArc: true,
+                      style: {
+                        fontSize: "35px",
+                        fill: "#fff",
+                        textShadow:
+                          "none 0px 0px 0px, black 0px 0px 0em, black 0px 0px 0em",
+                        border: "0px",
+                      },
+                    },
+                    tickLabels: {
+                      type: "outer",
+                      ticks: [
+                        { value: 25 },
+                        { value: 50 },
+                        { value: 75 },
+                        { value: 100 },
+                      ],
+                    },
+                  }}
+                  arc={{
+                    colorArray: ["#5BE12C", "#EA4228"],
+                    subArcs: [
+                      { limit: 25 },
+                      { limit: 50 },
+                      { limit: 75 },
+                      { limit: 100 },
+                    ],
+                    padding: 0.02,
+                    width: 0.3,
+                  }}
+                  pointer={{
+                    type: "arrow",
+                    elastic: true,
+                    animationDelay: 0,
+                  }}
+                />
+              </div>
               <Typography variant="body2" color="textSecondary" mb={1}>
                 <strong>Coordinates:</strong> lat 71.243, long 46.434
               </Typography>
